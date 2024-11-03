bd2c4ef11d0d213b3eb0951b715c308d6c57116b
Luis Giraldo Santiago <luisgirsan@gmail.com>
2024-11-03 14:18:46 +0100

    fix(socialModule): se añaden los atributos de la clase socialmodule. #1

--- app/modules/socialmodule/models.py
+++ app/modules/socialmodule/models.py
@@ -1,13 +1,16 @@
 from app import db
+from datetime import datetime, timezone
 
 
 class SocialModule(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    follower = db.relationship('User', backref='social_model', lazy=True, cascade="all, delete")
-    followed = db.relationship('User', backref='social_model', lazy=True, cascade="all, delete")
-
+    text = db.Column(db.String(256), nullable=False)
+    comment = db.Column(db.Boolean, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
+    follower = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    followed = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    data_set = db.Column(db.Integer, db.ForeignKey('data_set.id'), nullable=True)
+    
     def __repr__(self):
         return f'SocialModule<{self.id}>'
-
-
 
