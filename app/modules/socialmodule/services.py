e4f20f6130f4d9b863d2450a613aee5bfa4705e9
Luis Giraldo Santiago <luisgirsan@gmail.com>
2024-11-03 14:19:14 +0100

    fix(socialModule): se añaden una entreda en la base de datos de la clase socialmodule. #1

--- app/modules/socialmodule/services.py
+++ app/modules/socialmodule/services.py
@@ -1,4 +1,3 @@
-from app.modules.hubfile.services import HubfileService
 from core.services.BaseService import BaseService
 from app.modules.socialmodule.repositories import SocialModuleRepository
 
@@ -6,7 +5,6 @@ from app.modules.socialmodule.repositories import SocialModuleRepository
 class FeatureModelService(BaseService):
     def __init__(self):
         super().__init__(SocialModuleRepository())
-        self.hubfile_service = HubfileService()
 
     def total_social_module_views(self) -> int:
         return self.hubfile_service.total_hubfile_views()
