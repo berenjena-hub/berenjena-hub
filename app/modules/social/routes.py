09bd5aa4b16f18ea4559c7db096e190c35e53c48
Luis Giraldo Santiago <luisgirsan@gmail.com>
2024-11-16 14:20:32 +0100

    fix(socialModule): Se añaden funciones para el envio y la busqueda de mensajes. #4

--- app/modules/social/routes.py
+++ app/modules/social/routes.py
@@ -1,18 +1,40 @@
-from flask import render_template, redirect, url_for, flash, request
+from flask import render_template, redirect, url_for, flash, request, jsonify
 from flask_login import login_required, current_user
-from datetime import datetime, timezone
 from app import db
 from app.modules.auth.models import User
-from app.modules.dataset.models import DataSet
-from app.modules.social.models import Social, Follow
 from app.modules.social import social_bp
-from app.modules.social.forms import SocialForm, FollowForm
 from app.modules.social.services import SocialService, FollowService
 
 
 @social_bp.route('/social', methods=['GET'])
 def index():
-    return render_template('social/index.html')
+    follow_service = FollowService()
+    friends_data = follow_service.get_follow_between(current_user.id)
+    friends_ids = friends_data[0]
+    friends = db.session.query(User).filter(User.id.in_(friends_ids)).all()
+    return render_template('social/index.html', friends=friends)
+
+
+@social_bp.route('/get_messages', methods=['GET'])
+@login_required
+def get_messages():
+    social_service = SocialService()
+    followed_id = request.args.get('friend_id')
+    messages = social_service.fetch_messages(current_user.id, followed_id)
+    print(messages)
+    return jsonify(messages)
+
+
+@social_bp.route('/send_message', methods=['POST'])
+@login_required
+def send_message():
+    social_service = SocialService()
+    data = request.json
+    follower_id = current_user.id
+    followed_id = data['followed_id']
+    text = data['text']
+    social_service.send_message(follower_id, followed_id, text)
+    return '', 204
 
 
 @social_bp.route('/follow/<int:user_id>', methods=['POST'])
