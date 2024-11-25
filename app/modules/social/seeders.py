70e7d794aec12bd3631526e760b2c1f41fd452ea
Luis Giraldo Santiago <luisgirsan@gmail.com>
2024-11-25 17:33:18 +0100

    fix(socialModule): Se añade comentarios a la base de datos comentarios. #3

--- app/modules/social/seeders.py
+++ app/modules/social/seeders.py
@@ -2,6 +2,7 @@ from core.seeders.BaseSeeder import BaseSeeder
 from app.modules.social.models import Social, Follow
 from datetime import datetime, timezone
 from app.modules.auth.models import User
+from app.modules.dataset.models import DataSet
 
 
 class SocialSeeder(BaseSeeder):
@@ -10,6 +11,8 @@ class SocialSeeder(BaseSeeder):
         
         user1 = User.query.filter_by(email='user1@example.com').first()
         user2 = User.query.filter_by(email='user2@example.com').first()
+        dataset_ids = [dataset.id for dataset in DataSet.query.all()]
+
 
         if not user1 or not user2:
             raise Exception("Users not found. Please seed users first.")
@@ -40,6 +43,20 @@ class SocialSeeder(BaseSeeder):
         ]
 
         self.seed(social)
+        
+        comments = [
+            Social(
+                text='Me gusta este dataset.',
+                comment=True,
+                created_at=datetime.now(timezone.utc),
+                follower=user1.id,  # Asegúrate de tener los IDs de los usuarios
+                followed=user2.id,
+                data_set=dataset_id  # Asignar el ID del dataset actual
+            )
+            for dataset_id in dataset_ids
+        ]
+
+        self.seed(comments)
 
         follow = [
             Follow(
