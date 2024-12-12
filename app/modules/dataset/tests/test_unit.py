f886c38ad718cb01c6d40f883672f527572c49b9
Felipe Solis Agudo <felipesolisagudo@gmail.com>
2024-12-12 18:28:06 +0100

    fix(improveUI): Arreglo tests unitarios. #57

--- app/modules/dataset/tests/test_unit.py
+++ app/modules/dataset/tests/test_unit.py
@@ -7,11 +7,12 @@ from werkzeug.exceptions import NotFound
 
 
 @pytest.fixture
-def app2():
-    app2 = create_app()  
-    app2.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'  
-    app2.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    return app2
+def app():
+    app = create_app()  
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'  
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    return app
+
 
 @pytest.fixture
 def rating_service():
@@ -19,18 +20,18 @@ def rating_service():
     return RatingService()
 
 
-def test_get_file_content(app2):
+def test_get_file_content(app):
     file_id = 1
     dataset_id = 1
 
-    with patch('app2.modules.dataset.services.DataSetService.get_or_404') as mock_get_or_404:
+    with patch('app.modules.dataset.services.DataSetService.get_or_404') as mock_get_or_404:
         mock_dataset = MagicMock()
         mock_dataset.id = dataset_id
         mock_dataset.name = "Dataset 1"
         mock_get_or_404.return_value = mock_dataset
         
-        with patch('app2.modules.dataset.routes.render_template') as mock_render_template:
-            with app2.app2_context():
+        with patch('app.modules.dataset.routes.render_template') as mock_render_template:
+            with app.app_context():
                 get_file_content(file_id, dataset_id)
 
             mock_render_template.assert_called_once_with(
@@ -40,15 +41,15 @@ def test_get_file_content(app2):
             )
       
             
-def test_get_file_content_not_found(app2):
+def test_get_file_content_not_found(app):
     file_id = 1
     dataset_id = 999 
 
-    with patch('app2.modules.dataset.services.DataSetService.get_or_404') as mock_get_or_404:
+    with patch('app.modules.dataset.services.DataSetService.get_or_404') as mock_get_or_404:
         mock_get_or_404.side_effect = NotFound()
 
-        with patch('app2.modules.dataset.routes.render_template') as mock_render_template:
-            with app2.app_context():
+        with patch('app.modules.dataset.routes.render_template') as mock_render_template:
+            with app.app_context():
                 try:
                     get_file_content(file_id, dataset_id)
                 except NotFound:
@@ -57,17 +58,17 @@ def test_get_file_content_not_found(app2):
             mock_render_template.assert_not_called()
           
 
-def test_delete_file(app2):
+def test_delete_file(app):
     file_name = "test_file.txt"
 
     file_path = os.path.join('/mock/temp/folder', file_name)
 
-    with patch('app2.modules.dataset.routes.current_user') as mock_user:
+    with patch('app.modules.dataset.routes.current_user') as mock_user:
         mock_user.temp_folder = MagicMock(return_value='/mock/temp/folder') 
 
         with patch('os.path.exists', return_value=True):
             with patch('os.remove') as mock_remove:
-                with app2.test_client() as client:
+                with app.test_client() as client:
                     response = client.post('/dataset/file/delete', json={"file": file_name})
 
                 mock_remove.assert_called_once_with(file_path)
@@ -76,22 +77,21 @@ def test_delete_file(app2):
                 assert response.json == {"message": "File deleted successfully"}
 
 
-def test_delete_file_negative(app2):
+def test_delete_file_negative(app):
     file_name = "test_file.txt"
 
-    with patch('app2.modules.dataset.routes.current_user') as mock_user:
+    with patch('app.modules.dataset.routes.current_user') as mock_user:
         mock_user.temp_folder = MagicMock(return_value='/mock/temp/folder')
     
         with patch('os.path.exists', return_value=False):
-            with app2.test_client() as client:
+            with app.test_client() as client:
                 response = client.post('/dataset/file/delete', json={"file": file_name})
 
             assert response.status_code == 200  
 
             assert response.json == {"error": "Error: File not found"}
    
-
-
+   
 def test_add_rating_successful(rating_service):
     """Verifica que se pueda agregar una calificación correctamente."""
     with patch.object(rating_service.repository, 'add_rating') as mock_add_rating:
