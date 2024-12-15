cac52d9153577e6706493d4f1fd75be2335b0f01
Luis Giraldo Santiago <luisgirsan@gmail.com>
2024-12-15 12:18:19 +0100

    fix(Luis-Giraldo-Santiago): Se cambia el modo de busqueda de un uvl para render. #63

--- app/modules/hubfile/routes.py
+++ app/modules/hubfile/routes.py
@@ -54,12 +54,53 @@ def view_file(file_id):
     file = HubfileService().get_or_404(file_id)
     filename = file.name
 
+    # Ruta local
     directory_path = f"uploads/user_{file.feature_model.data_set.user_id}/dataset_{file.feature_model.data_set_id}/"
     parent_directory_path = os.path.dirname(current_app.root_path)
     file_path = os.path.join(parent_directory_path, directory_path, filename)
 
+    # URL de GitHub
+    github_base_url = "https://raw.githubusercontent.com/berenjena-hub/files/refs/heads/main/uploads/"
+    user_id = file.feature_model.data_set.user_id
+    dataset_id = file.feature_model.data_set_id
+    github_file_url = f"{github_base_url}user_{user_id}/dataset_{dataset_id}/{filename}"
+
+
     try:
         if os.path.exists(file_path):
+            with open(file_path, 'r') as f:
+                content = f.read()
+
+            user_cookie = request.cookies.get('view_cookie')
+            if not user_cookie:
+                user_cookie = str(uuid.uuid4())
+
+            # Check if the view record already exists for this cookie
+            existing_record = HubfileViewRecord.query.filter_by(
+                user_id=current_user.id if current_user.is_authenticated else None,
+                file_id=file_id,
+                view_cookie=user_cookie
+            ).first()
+
+            if not existing_record:
+                # Register file view
+                new_view_record = HubfileViewRecord(
+                    user_id=current_user.id if current_user.is_authenticated else None,
+                    file_id=file_id,
+                    view_date=datetime.now(),
+                    view_cookie=user_cookie
+                )
+                db.session.add(new_view_record)
+                db.session.commit()
+
+            # Prepare response
+            response = jsonify({'success': True, 'content': content})
+            if not request.cookies.get('view_cookie'):
+                response = make_response(response)
+                response.set_cookie('view_cookie', user_cookie, max_age=60*60*24*365*2)
+
+            return response
+        elif os.path.exists(github_file_url):
             with open(file_path, 'r') as f:
                 content = f.read()
 
